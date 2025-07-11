559f868aa070af827090dd7c23212fbe3640e3ab
Georgia Smits <ges256@cornell.edu>
2025-07-11 16:36:03 -0400

    corrected sigma_e in fbtf function

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # funcBTF
+
+![](plots/plant_KV_plot.pdf)
+
+![](plots/plant_KV_segmented.pdf)
+
+![](plots/plant_KV_naiveDSP.pdf)
+
+![](plots/plant_KV_interpDSP.pdf)
+
+![](plots/plant_KV_fBTF_D2.pdf)
--- fBTF_funcs.R
+++ fBTF_funcs.R
@@ -59,7 +59,7 @@ rescale_evol_error <- function(mu,  # beta_i
 # Sampling functions ####
 t_sampleEvolParams <- function(omega, evolParams) { # takes already re-scaled evolution error as input
   nd <- length(omega)
-  tau <- 1/sqrt(nd) # maybe can fix tau universally if we pre-scale the data? - David
+  tau <- 0.001/sqrt(nd) # maybe can fix tau universally if we pre-scale the data? - David
   omega_norm <- omega/tau
   lambda_2 <- evolParams$lambda_2
   x_lambda_t <- extraDistr::rinvgamma(nd, 1, 1 + 1 / lambda_2)
@@ -70,7 +70,7 @@ t_sampleEvolParams <- function(omega, evolParams) { # takes already re-scaled ev
 
 t_initEvolParams <- function(omega) { # takes already re-scaled evolution error as input
   nd <- length(omega)
-  tau <- 1/sqrt(nd) # maybe can fix tau universally if we pre-scale the data? - David
+  tau <- 0.001/sqrt(nd) # maybe can fix tau universally if we pre-scale the data? - David
   omega_norm <- omega/tau
   x_lambda_t <- rep(100, nd)
   lambda_2 <- extraDistr::rinvgamma(nd, 1, 1 / x_lambda_t + omega_norm^2 / 2)
@@ -81,7 +81,7 @@ t_initEvolParams <- function(omega) { # takes already re-scaled evolution error 
 t_initEvolParams_no <- function(y, D, omega) { # takes already re-scaled evolution error as input
   n <- length(y)
   nd <- n-D
-  tau = 1/sqrt(nd) # maybe can fix tau universally if we pre-scale the data? - David
+  tau = 0.001/sqrt(nd) # maybe can fix tau universally if we pre-scale the data? - David
   omega_norm <- omega/tau
   x_lambda_t <- rep(100, nd)
   lambda_2 <- extraDistr::rinvgamma(nd, 1, 1 / x_lambda_t + omega_norm^2 / 2)
@@ -93,88 +93,70 @@ t_initEvolParams_no <- function(y, D, omega) { # takes already re-scaled evoluti
 fbtf <- function (y_k,        # original observations
                   t_k = NULL, # original observation times
                   D = 2,      # degree of differencing
-                  evol_error = "DHS", useObsSV = FALSE,
                   nsave = 1000, nburn = 1000, nskip = 4,
                   mcmc_params = list("mu", "yhat",
                                      "evol_sigma_t2", "obs_sigma_t2",
                                      "dhs_phi", "dhs_mean"),
                   computeDIC = TRUE, verbose = TRUE) {
+  
   # preprocessing and initialization
-  {
-    evol_error = toupper(evol_error)
-    if (D == 0) {
-      stop('D must be 1 or 2') # will modify btf0 later, not really needed at the moment
-      # return(fbtf0(y = y, evol_error = evol_error, useObsSV = useObsSV, 
-      #              nsave = nsave, nburn = nburn, nskip = nskip, mcmc_params = mcmc_params, 
-      #              computeDIC = computeDIC, verbose = verbose))
-    }
-    
-    if (is.null(t_k)) {t_k <- seq_along(y_k)} # default equally spaced times
-    
-    # remove ties
-    tie_adj_dat <- preprocess_ties(y_k, t_k)
-    y <- tie_adj_dat$y
-    t <- tie_adj_dat$t
-    m <- tie_adj_dat$m
-    n <- length(y)
-    
-    deltas <- get_deltas(t, D)
-    delta_sq <- if (is.null(deltas)) {NULL} else {deltas^2}
-    
-    loc <- t_create_loc(n - D, 1)
-    
-    is.missing <- which(is.na(y))
-    any.missing <- (length(is.missing) > 0)
-    y <- approxfun(t, y, rule = 2)(t)
-    
-    # shrink sigma by square root of number of ties
-    sigma_e <- sd(y, na.rm = TRUE) / sqrt(m)
-    sigma_et <- sigma_e
-    
-    chol0 <- NULL
-    
-    evol_sigma_t2_init <- 0.01 * sigma_et^2
-    if (D > 0){
-      evol_sigma_t2_init[(D+1):n] <- evol_sigma_t2_init[(D+1):n] * delta_sq
-    }
-    
-    mu <- dsp::sampleBTF(y, obs_sigma_t2 = sigma_et^2,
-                         evol_sigma_t2 = evol_sigma_t2_init,
-                         D = D, chol0 = chol0)
-    print("no errors yet")
-    
-    # rescale evolution error
-    omega <- rescale_evol_error(mu, t, D)
-    mu0 <- as.matrix(mu[1:D, ])
-    evolParams <- t_initEvolParams_no(y, D, omega)
-    evolParams0 <- initEvol0(mu0)
-    
-    if (useObsSV) {
-      svParams = initSV(y - mu)
-      sigma_et = svParams$sigma_wt
-    }
-    
-    mcmc_output <- vector("list", length(mcmc_params))
-    names(mcmc_output) <- mcmc_params
-    if (!is.na(match("mu", mcmc_params)) || computeDIC) 
-      post_mu <- array(NA, c(nsave, n))
-    if (!is.na(match("yhat", mcmc_params))) 
-      post_yhat <- array(NA, c(nsave, n))
-    if (!is.na(match("obs_sigma_t2", mcmc_params)) || computeDIC) 
-      post_obs_sigma_t2 <- array(NA, c(nsave, n))
-    if (!is.na(match("evol_sigma_t2", mcmc_params))) 
-      post_evol_sigma_t2 <- array(NA, c(nsave, n))
-    # if (!is.na(match("dhs_phi", mcmc_params)) && evol_error == "DHS") 
-    #   post_dhs_phi <- numeric(nsave)
-    # if (!is.na(match("dhs_mean", mcmc_params)) && evol_error == "DHS") 
-    #   post_dhs_mean <- numeric(nsave)
-    post_loglike <- numeric(nsave)
-    
-    nstot <- nburn + (nskip + 1) * (nsave)
-    skipcount <- 0
-    isave <- 0
-    if (verbose) {timer0 = proc.time()[3]}
+  if (D == 0) {stop('D must be 1 or 2')} # will modify btf0 later, not really needed at the moment
+  
+  if (is.null(t_k)) {t_k <- seq_along(y_k)} # default equally spaced times
+  
+  # remove ties
+  tie_adj_dat <- preprocess_ties(y_k, t_k)
+  y <- tie_adj_dat$y
+  t <- tie_adj_dat$t
+  m <- tie_adj_dat$m
+  n <- length(y)
+  
+  deltas <- get_deltas(t, D)
+  delta_sq <- if (is.null(deltas)) {NULL} else {deltas^2}
+  
+  loc <- t_create_loc(n - D, 1)
+  
+  is.missing <- which(is.na(y))
+  any.missing <- (length(is.missing) > 0)
+  y <- approxfun(t, y, rule = 2)(t)
+  
+  # shrink sigma by square root of number of ties
+  sigma_e <- sd(y, na.rm = TRUE) / sqrt(m)
+  sigma_et <- sigma_e
+  
+  chol0 <- NULL
+  
+  evol_sigma_t2_init <- 0.01 * sigma_et^2
+  if (D > 0){
+    evol_sigma_t2_init[(D+1):n] <- evol_sigma_t2_init[(D+1):n] * delta_sq
   }
+  
+  mu <- dsp::sampleBTF(y, obs_sigma_t2 = sigma_et^2,
+                       evol_sigma_t2 = evol_sigma_t2_init,
+                       D = D, chol0 = chol0)
+  
+  # re-scale evolution error
+  omega <- rescale_evol_error(mu, t, D)
+  mu0 <- as.matrix(mu[1:D, ])
+  evolParams <- t_initEvolParams_no(y, D, omega)
+  evolParams0 <- initEvol0(mu0)
+  
+  mcmc_output <- vector("list", length(mcmc_params))
+  names(mcmc_output) <- mcmc_params
+  if (!is.na(match("mu", mcmc_params)) || computeDIC) 
+    post_mu <- array(NA, c(nsave, n))
+  if (!is.na(match("yhat", mcmc_params))) 
+    post_yhat <- array(NA, c(nsave, n))
+  if (!is.na(match("obs_sigma_t2", mcmc_params)) || computeDIC) 
+    post_obs_sigma_t2 <- array(NA, c(nsave, n))
+  if (!is.na(match("evol_sigma_t2", mcmc_params))) 
+    post_evol_sigma_t2 <- array(NA, c(nsave, n))
+  post_loglike <- numeric(nsave)
+  
+  nstot <- nburn + (nskip + 1) * (nsave)
+  skipcount <- 0
+  isave <- 0
+  if (verbose) {timer0 = proc.time()[3]}
   
   # sampling
   for (nsi in seq_len(nstot)) {
@@ -190,17 +172,9 @@ fbtf <- function (y_k,        # original observations
     mu0 <- as.matrix(mu[1:D, ])
     evolParams0 <- sampleEvol0(mu0, evolParams0, A = 1)
     evolParams <- t_sampleEvolParams(omega, evolParams)
-    if (nsi <= 5) {print("not broken yet")}
-    
-    if (useObsSV) {
-      svParams <- sampleSVparams(omega = y - mu, svParams = svParams)
-      sigma_et <- svParams$sigma_wt
-    } else {
-      sigma_e <- 1/sqrt(rgamma(n = 1, shape = n/2 + length(c(mu0, omega))/2,
-                               rate  = sum((y - mu)^2)/2 +
-                                 sum((c(mu0, omega) / c(evolParams0$sigma_w0, evolParams$sigma_wt))^2)/2))
-      sigma_et <- rep(sigma_e, n)
-    }
+    sigma_e <- sqrt(extraDistr::rinvgamma(n = 1, alpha = n/2, 
+                                          beta = sum(m*(y-mu)^2)/2))
+    sigma_et <- rep(sigma_e, n)
     
     if (nsi > nburn) {
       skipcount = skipcount + 1
@@ -215,10 +189,6 @@ fbtf <- function (y_k,        # original observations
         if (!is.na(match("evol_sigma_t2", mcmc_params))) 
           post_evol_sigma_t2[isave, ] <- c(evolParams0$sigma_w0^2, 
                                            evolParams$sigma_wt^2 * if (D > 0) delta_sq else NULL)
-        # if (!is.na(match("dhs_phi", mcmc_params)) && evol_error == "DHS") 
-        #   post_dhs_phi[isave] <- evolParams$dhs_phi
-        # if (!is.na(match("dhs_mean", mcmc_params)) && evol_error == "DHS") 
-        #   post_dhs_mean[isave] <- evolParams$dhs_mean
         post_loglike[isave] = sum(dnorm(y, mu, sigma_et, log = TRUE))
         skipcount = 0
       }
@@ -235,10 +205,6 @@ fbtf <- function (y_k,        # original observations
     mcmc_output$obs_sigma_t2 <- post_obs_sigma_t2
   if (!is.na(match("evol_sigma_t2", mcmc_params))) 
     mcmc_output$evol_sigma_t2 <- post_evol_sigma_t2
-  # if (!is.na(match("dhs_phi", mcmc_params)) && evol_error == "DHS")
-  #   mcmc_output$dhs_phi <- post_dhs_phi
-  # if (!is.na(match("dhs_mean", mcmc_params)) && evol_error == "DHS")
-  #   mcmc_output$dhs_mean <- post_dhs_mean
   mcmc_output$loglike <- post_loglike
   if (computeDIC) {
     loglike_hat <- sum(dnorm(y, mean = colMeans(post_mu), 
@@ -254,6 +220,67 @@ fbtf <- function (y_k,        # original observations
   return(mcmc_output)
 }
 
+# Unedited functions ####
+# From dspCP
+t_create_loc <- function (len, D) {
+  if (D == 0 || D == 1) {
+    row_ind = c()
+    col_ind = c()
+    for (i in 0:(len - 1)) {
+      row_ind = c(row_ind, i)
+      col_ind = c(col_ind, i)
+    }
+    for (i in 0:(len - 2)) {
+      row_ind = c(row_ind, i)
+      col_ind = c(col_ind, i + 1)
+    }
+    for (i in 0:(len - 2)) {
+      row_ind = c(row_ind, i + 1)
+      col_ind = c(col_ind, i)
+    }
+  }
+  if (D == 2) {
+    row_ind = c()
+    col_ind = c()
+    for (i in 0:(len - 1)) {
+      row_ind = c(row_ind, i)
+      col_ind = c(col_ind, i)
+    }
+    for (i in 0:(len - 2)) {
+      row_ind = c(row_ind, i)
+      col_ind = c(col_ind, i + 1)
+    }
+    for (i in 0:(len - 2)) {
+      row_ind = c(row_ind, i + 1)
+      col_ind = c(col_ind, i)
+    }
+    for (i in 0:(len - 3)) {
+      row_ind = c(row_ind, i)
+      col_ind = c(col_ind, i + 2)
+    }
+    for (i in 0:(len - 3)) {
+      row_ind = c(row_ind, i + 2)
+      col_ind = c(col_ind, i)
+    }
+  }
+  list(r = row_ind, c = col_ind)
+}
 
-
+computeTimeRemaining <- function (nsi, timer0, nsims, nrep = 1000) 
+{
+  if (nsi%%nrep == 0 || nsi == 20) {
+    timer = proc.time()[3]
+    simsPerSec = nsi/(timer - timer0)
+    secRemaining = (nsims - nsi - 1)/simsPerSec
+    if (secRemaining > 3600) {
+      print(paste(round(secRemaining/3600, 1), "hours remaining"))
+    }
+    else {
+      if (secRemaining > 60) {
+        print(paste(round(secRemaining/60, 2), "minutes remaining"))
+      }
+      else print(paste(round(secRemaining, 2), "seconds remaining"))
+    }
+  }
+}
 
